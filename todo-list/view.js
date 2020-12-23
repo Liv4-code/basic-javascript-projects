d0a12c309430859665a803a10f7243950316fd76
livi <scholtz.ice@gmail.com>
2020-12-23 12:02:05 +0200

    Todo items can be added and added a checkbox and delete button to each list item.

--- todo-list/view.js
+++ todo-list/view.js
@@ -1,3 +1,15 @@
-const addInput = document.querySelector("#add");
-const searchInput = document.querySelector("#search");
+const addInput = document.querySelector("#addForm");
+const addButton = document.querySelector("#addButton");
+const todoList = document.querySelector("#todoList");
+
+addInput.addEventListener("submit", e => {
+
+    e.preventDefault();
+
+    const inputValue = document.querySelector("#addInput").value;
+    console.log(inputValue);
+    todoList.innerHTML += `<span><li>${inputValue}<input class="option checkbox" type="checkbox"></li><button class="option">X</button></span>`;
+});
+
+
 
