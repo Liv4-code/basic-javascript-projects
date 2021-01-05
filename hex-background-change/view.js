7e885d3cd3daa490241b1715b08b44ebb2684b06
livi <scholtz.ice@gmail.com>
2021-01-05 21:18:11 +0200

    Added html and js file with basic structure and styling.

---
+++ hex-background-change/view.js
@@ -0,0 +1,6 @@
+const hexCodes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const random = Math.floor(Math.random() * 16);
+
+console.log(random);
+
+// Return a single random array item.
