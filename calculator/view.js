c8cc4dc00ab4250ef1d62665ec0b35193d8138f9
livi <scholtz.ice@gmail.com>
2021-11-26 15:15:55 +0200

    Conditional activities for delete and equals buttons.

--- calculator/view.js
+++ calculator/view.js
@@ -1,14 +1,20 @@
 const calcButtons = document.querySelector(".calc-buttons");
 const numButtons = document.querySelectorAll(".number");
+const deleteButton = document.querySelector(".delete");
 const decimalPoint = document.querySelector(".decimal");
+const equalsButton = document.querySelector(".equals");
 const opButtons = document.querySelectorAll(".operation");
 
 calcButtons.addEventListener("click", (e) => {
-    const selected = e.target.textContent;
-
-    const output = (document.querySelector("output").innerHTML += selected);
-
-    console.log(output);
+    if (e.target === deleteButton) {
+        console.log("You clicked the delete button");
+    } else if (e.target === equalsButton) {
+        console.log("Equals?...");
+    } else {
+        const selected = e.target.textContent;
+        const output = (document.querySelector("output").innerHTML += selected);
+        return output;
+    }
 });
 
 // Capture output
