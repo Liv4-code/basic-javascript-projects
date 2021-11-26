92097ae42ef7e59524dad8daa7d44a77aed594a4
livi <scholtz.ice@gmail.com>
2021-11-26 14:27:35 +0200

    Styled output.

--- calculator/view.js
+++ calculator/view.js
@@ -1,2 +1,9 @@
 const calcButtons = document.querySelector(".calculator");
-const output = document.querySelector("output");
+
+calcButtons.addEventListener("click", (e) => {
+    let output = document.querySelector("output").innerHTML;
+    const selected = e.target.textContent;
+});
+
+// Capture output
+// Change output each time user clicks on a button
